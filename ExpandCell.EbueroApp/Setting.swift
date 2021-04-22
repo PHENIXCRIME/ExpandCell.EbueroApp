725257a44f213434881f0426c29b267dfd973a72
Waranyu Khongchai <waranyu.khongchai@clbs.co.th>
2021-04-22 14:01:51 +0700

    CREATE file Setting for object Data

---
+++ ExpandCell.EbueroApp/Setting.swift
@@ -0,0 +1,18 @@
+//
+//  Setting.swift
+//  ExpandCell.EbueroApp
+//
+//  Created by PHENIXCRIME on 22/4/2564 BE.
+//
+
+import Foundation
+
+public enum TypeSetting {
+    case settingCell
+    case headerCell
+    case bottomCell
+}
+
+public struct SettingData {
+    
+}
