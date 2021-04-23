c493126fd6614b9c672129004bedf2c2ebd67260
Waranyu Khongchai <waranyu.khongchai@clbs.co.th>
2021-04-23 13:51:42 +0700

    EDIT set delegate, dataSource, registerCell & set func numberOfRowInSection, cellForRowAt and heightForRowAt

--- ExpandCell.EbueroApp/ViewController.swift
+++ ExpandCell.EbueroApp/ViewController.swift
@@ -7,15 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableViewSetting: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        prepareViews()
+        registerCell()
+        setTypeSetting()
+        
+        tableViewSetting.delegate = self
+        tableViewSetting.dataSource = self
+        
+        tableViewSetting.separatorStyle = .none
+        tableViewSetting.rowHeight = UITableView.automaticDimension
     }
-
-
+    
+    func prepareViews() {
+        
+    }
+    
+    func registerCell() {
+        let settingCell = UINib(nibName: "settingCell", bundle: Bundle.main)
+        let headerDetailCell = UINib(nibName: "headerDetailCell", bundle: Bundle.main)
+        let bottomDetailCell = UINib(nibName: "bottomDetailCell", bundle: Bundle.main)
+        
+        tableViewSetting.register(settingCell, forCellReuseIdentifier: "settingCell")
+        tableViewSetting.register(headerDetailCell, forCellReuseIdentifier: "headerDetailCell")
+        tableViewSetting.register(bottomDetailCell, forCellReuseIdentifier: "bottomDetailCell")
+        
+    }
+    
+    func setTypeSetting() {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        <#code#>
+    }
+    
 }
 
