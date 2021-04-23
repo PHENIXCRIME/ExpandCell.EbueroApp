e1eb14dd815ec5da28763f209d3398435da2f537
Waranyu Khongchai <waranyu.khongchai@clbs.co.th>
2021-04-23 15:14:24 +0700

    EDIT dataSetting, numberOfRowsInSection, cellForRowAt as setting.type and heightForRowAt as type of setting

--- ExpandCell.EbueroApp/ViewController.swift
+++ ExpandCell.EbueroApp/ViewController.swift
@@ -42,28 +42,58 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func setTypeSetting() {
-        dataSetting = [SettingData(icon: "", setting: "Secretary's profile", type: .settingCell),
-                       SettingData(icon: "", setting: "About your company", type: .settingCell),
-                       SettingData(icon: "", setting: "Forwarding calls to You", type: .settingCell),
-                       SettingData(icon: "", setting: "Forwarding calls to Us", type: .settingCell),
-                       SettingData(icon: "", setting: "How to notify you", type: .settingCell),
-                       SettingData(icon: "", setting: "Calendar settings", type: .settingCell),
-                       SettingData(icon: "", setting: "Addressbook settings", type: .settingCell),
-                       SettingData(icon: "", setting: "User settings", type: .settingCell),
-                       SettingData(icon: "", setting: "Privacy settings", type: .settingCell),
-                       SettingData(icon: "", setting: "App settings", type: .settingCell)]
+        dataSetting = [SettingData(icon: "", textSetting: "Secretary's profile", type: .settingCell),
+                       SettingData(icon: "", textSetting: "About your company", type: .settingCell),
+                       SettingData(icon: "", textSetting: "Forwarding calls to You", type: .settingCell),
+                       SettingData(icon: "", textSetting: "Forwarding calls to Us", type: .settingCell),
+                       SettingData(icon: "", textSetting: "How to notify you", type: .settingCell),
+                       SettingData(icon: "", textSetting: "Calendar settings", type: .settingCell),
+                       SettingData(icon: "", textSetting: "Addressbook settings", type: .settingCell),
+                       SettingData(icon: "", textSetting: "User settings", type: .settingCell),
+                       SettingData(icon: "", textSetting: "Privacy settings", type: .settingCell),
+                       SettingData(icon: "", textSetting: "App settings", type: .settingCell)]
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return dataSetting.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let setting = dataSetting[indexPath.row]
+        
+        if setting.type == .settingCell {
+            let cell = tableViewSetting.dequeueReusableCell(withIdentifier: settingCell.identifier, for: indexPath) as! settingCell
+            cell.txSetting.text = setting.textSetting
+            if let icon = setting.icon {
+                cell.iconSetting.image = UIImage(named: icon)?.withRenderingMode(.alwaysTemplate)
+            }
+            return cell
+        } else if setting.type == .headerDetailCell {
+            let cell = tableViewSetting.dequeueReusableCell(withIdentifier: headerDetailCell.identifier, for: indexPath) as! headerDetailCell
+            cell.txDetailHeader.text = setting.detailHeader
+            if let image = setting.imageHeader {
+                cell.imageHeader.image = UIImage(named: image)?.withRenderingMode(.alwaysOriginal)
+            }
+            return cell
+        } else {
+            let cell = tableViewSetting.dequeueReusableCell(withIdentifier: bottomDetailCell.identifier, for: indexPath) as! bottomDetailCell
+            cell.txSubSetting.text = setting.subSetting
+            return cell
+        }
     }
+        
+    
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        <#code#>
+        let setting = dataSetting[indexPath.row]
+        switch setting.type {
+        case .settingCell:
+            return 40
+        case .headerDetailCell:
+            return 160
+        case .bottomDetailCell:
+            return 40
+        }
     }
     
 }
