a8e3777095504edc8959870f169342461c4438af
Waranyu Khongchai <waranyu.khongchai@clbs.co.th>
2021-04-26 15:14:09 +0700

    CREATE cell xib cardDeatilSetting

---
+++ ExpandCell.EbueroApp/CellSetting/CardCell/cardDetailSetting.swift
@@ -0,0 +1,23 @@
+//
+//  cardDetailSetting.swift
+//  ExpandCell.EbueroApp
+//
+//  Created by PHENIXCRIME on 26/4/2564 BE.
+//
+
+import UIKit
+
+class cardDetailSetting: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
