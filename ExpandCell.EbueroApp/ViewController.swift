44a32de37ba2b96ec2a48b3d26259fd6e7ed4dd7
Waranyu Khongchai <waranyu.khongchai@clbs.co.th>
2021-04-30 12:04:49 +0700

    EDIT add cardDetailSettingCellDelegate

--- ExpandCell.EbueroApp/ViewController.swift
+++ ExpandCell.EbueroApp/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, cardDetailSettingDelegate{
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, cardDetailSettingDelegate ,cardDetailSettingCellDelegate {
     
     @IBOutlet weak var tableViewSetting: UITableView!
     
