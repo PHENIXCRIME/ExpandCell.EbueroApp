ef8bd4ebd9e36b260775e76dc061f4c5cf4da2db
Waranyu Khongchai <waranyu.khongchai@clbs.co.th>
2021-05-05 17:58:00 +0700

    EDIT add IBOutlet element , set identifier

--- ExpandCell.EbueroApp/CellSetting/SettingMenuCell.swift
+++ ExpandCell.EbueroApp/CellSetting/SettingMenuCell.swift
@@ -8,16 +8,24 @@
 import UIKit
 
 class SettingMenuCell: UITableViewCell {
+    @IBOutlet weak var viewSettingCell: UIView!
+    @IBOutlet weak var imgIconSeting: UIImage!
+    @IBOutlet weak var txSetting: UILabel!
+    @IBOutlet weak var btnExpanable: UIButton!
+    
+    static let identifier = "SettingMenuCell"
+    
+    static func nib() ->UINib {
+        return UINib(nibName: "SettingMenuCell", bundle: nil)
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        viewSettingCell.layer.cornerRadius = 5
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
