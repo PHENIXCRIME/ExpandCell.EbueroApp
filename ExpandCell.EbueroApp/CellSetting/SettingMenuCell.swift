865a1c86e7bcedd3107c60c7a0dba8a2d9d6e368
Waranyu Khongchai <waranyu.khongchai@clbs.co.th>
2021-05-05 16:54:33 +0700

    CREATE SettingMenuCell, MenuCardDetailCell, CardDetailCell, SettingDetailCell

---
+++ ExpandCell.EbueroApp/CellSetting/SettingMenuCell.swift
@@ -0,0 +1,23 @@
+//
+//  SettingMenuCell.swift
+//  ExpandCell.EbueroApp
+//
+//  Created by PHENIXCRIME on 5/5/2564 BE.
+//
+
+import UIKit
+
+class SettingMenuCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
